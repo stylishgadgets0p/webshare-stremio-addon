8aa2b8bd4d6046e6f9c4a3cc5782d9f4f176bd12
Jonas Trantina <jonas.trantina@altworx.com>
2025-05-23 11:20:56 +0200

    Add tests for search results formatting

--- test/webshare.test.js
+++ test/webshare.test.js
@@ -160,3 +160,233 @@ describe("search results are sorted", () => {
     ]);
   });
 });
+
+describe("formatting description and name", () => {
+  test("description and name are formed correctly from the filename and metadata", async () => {
+    const showInfo = {
+      name: null,
+      nameSk: null,
+      originalName: "The Relic",
+      type: "movie",
+      year: "1999",
+    };
+
+    needle.mockImplementation((method, url, data, headers) => ({
+      body: {
+        children: [
+          file(
+            "1",
+            "The.Relic.1999.1080p.BluRay.x264-[YTS.AG].mkv",
+            "2000000000",
+            "10",
+            "0",
+          ),
+          file(
+            "2",
+            "The.Relic.1999.2160p.UHD.BluRay.x265.10bit.HDR.DTS-HD.MA.5.1-SWTYBLZ.mkv",
+            "30000000000",
+            "15",
+            "0",
+          ),
+          file(
+            "3",
+            "The.Relic.1999.REMASTERED.1080p.BluRay.H264.AAC-RARBG.mp4",
+            "2500000000",
+            "8",
+            "0",
+          ),
+          file(
+            "4",
+            "The.Relic.1999.1080p.WEB-DL.DD5.1.H264-FGT.mkv",
+            "180000000",
+            "12",
+            "0",
+          ),
+          file(
+            "5",
+            "The.Relic.1999.720p.BluRay.x264-[YTS.AG].mkv",
+            "1000000000",
+            "5",
+            "0",
+          ),
+        ],
+      },
+    }));
+
+    const results = await webshare.search(showInfo, null);
+    expect(results.length).toBe(5);
+    // Results include the filename, the votes and the language in decsription
+    expect(results[0].description).toContain("👍 15 👎 0");
+    expect(results[0].description).toContain(
+      "The.Relic.1999.2160p.UHD.BluRay.x265.10bit.HDR.DTS-HD.MA.5.1-SWTYBLZ.mkv",
+    );
+    expect(results[0].description).toContain("💾 30 GB");
+    expect(results[0].name).toBe("Webshare ✅ 2160p");
+
+    expect(results[1].description).toContain("👍 12 👎 0");
+    expect(results[1].description).toContain(
+      "The.Relic.1999.1080p.WEB-DL.DD5.1.H264-FGT.mkv",
+    );
+    expect(results[1].description).toContain("💾 180 MB");
+    expect(results[1].name).toBe("Webshare ✅ 1080p");
+
+    expect(results[2].description).toContain("👍 10 👎 0");
+    expect(results[2].description).toContain(
+      "The.Relic.1999.1080p.BluRay.x264-[YTS.AG].mkv",
+    );
+    expect(results[2].description).toContain("💾 2 GB");
+    expect(results[2].name).toBe("Webshare ✅ 1080p");
+
+    expect(results[3].description).toContain("👍 8 👎 0");
+    expect(results[3].description).toContain(
+      "The.Relic.1999.REMASTERED.1080p.BluRay.H264.AAC-RARBG.mp4",
+    );
+    expect(results[3].description).toContain("💾 2.5 GB");
+    expect(results[3].name).toBe("Webshare ✅ 1080p");
+
+    expect(results[4].description).toContain("👍 5 👎 0");
+    expect(results[4].description).toContain(
+      "The.Relic.1999.720p.BluRay.x264-[YTS.AG].mkv",
+    );
+    expect(results[4].description).toContain("💾 1 GB");
+    expect(results[4].name).toBe("Webshare ✅ 720p");
+  });
+
+  test("handles common TV show filename patterns", async () => {
+    const showInfo = {
+      name: null,
+      nameSk: null,
+      originalName: "Rolling Rad",
+      type: "series",
+      series: "1",
+      episode: "1",
+      year: null,
+    };
+
+    needle.mockImplementation((method, url, data, headers) => ({
+      body: {
+        children: [
+          file(
+            "1",
+            "Rolling.Rad.S01E01.1080p.BluRay.x264-[YTS.AG].mkv",
+            "2000000000",
+            "10",
+            "0",
+          ),
+          file(
+            "2",
+            "Rolling.Rad.S01E01.2160p.UHD.BluRay.x265.10bit.HDR.DTS-HD.MA.5.1-SWTYBLZ.mkv",
+            "30000000000",
+            "15",
+            "0",
+          ),
+          file(
+            "3",
+            "Rolling.Rad.S01E01.REMASTERED.1080p.BluRay.H264.AAC-RARBG.mp4",
+            "2500000000",
+            "8",
+            "0",
+          ),
+          file(
+            "5",
+            "Rolling.Rad.S01E01.720p.BluRay.x264-[YTS.AG].mkv",
+            "1000000000",
+            "5",
+            "0",
+          ),
+        ],
+      },
+    }));
+
+    const results = await webshare.search(showInfo, null);
+    expect(results.length).toBe(4);
+    expect(results[0].description).toContain("👍 15 👎 0");
+    expect(results[0].description).toContain(
+      "Rolling.Rad.S01E01.2160p.UHD.BluRay.x265.10bit.HDR.DTS-HD.MA.5.1-SWTYBLZ.mkv",
+    );
+    expect(results[0].description).toContain("💾 30 GB");
+    expect(results[0].name).toBe("Webshare ✅ 2160p");
+
+    expect(results[1].description).toContain("👍 10 👎 0");
+    expect(results[1].description).toContain(
+      "Rolling.Rad.S01E01.1080p.BluRay.x264-[YTS.AG].mkv",
+    );
+    expect(results[1].description).toContain("💾 2 GB");
+    expect(results[1].name).toBe("Webshare ✅ 1080p");
+
+    expect(results[2].description).toContain("👍 8 👎 0");
+    expect(results[2].description).toContain(
+      "Rolling.Rad.S01E01.REMASTERED.1080p.BluRay.H264.AAC-RARBG.mp4",
+    );
+    expect(results[2].description).toContain("💾 2.5 GB");
+    expect(results[2].name).toBe("Webshare ✅ 1080p");
+
+    expect(results[3].description).toContain("👍 5 👎 0");
+    expect(results[3].description).toContain(
+      "Rolling.Rad.S01E01.720p.BluRay.x264-[YTS.AG].mkv",
+    );
+    expect(results[3].description).toContain("💾 1 GB");
+    expect(results[3].name).toBe("Webshare ✅ 720p");
+  });
+
+  test("handles filenames with language and subtitle information", async () => {
+    const showInfo = {
+      name: null,
+      nameSk: null,
+      originalName: "The LongTrain",
+      type: "movie",
+      year: "1999",
+    };
+
+    needle.mockImplementation((method, url, data, headers) => ({
+      body: {
+        children: [
+          file(
+            "1",
+            "The.LongTrain.1999.1080p.BluRay.x264-[YTS.AG].mkv",
+            "2000000000",
+            "5",
+            "0",
+          ),
+          file(
+            "2",
+            "The.LongTrain.1999.1080p.BluRay.x264-[YTS.AG].CZ.EN.mkv",
+            "2000000000",
+            "4",
+            "0",
+          ),
+          file(
+            "3",
+            "The.LongTrain.1999.1080p.BluRay.x264-[YTS.AG].EN.CZ.mkv",
+            "2000000000",
+            "3",
+            "0",
+          ),
+          file(
+            "4",
+            "The.LongTrain.1999.1080p.BluRay.x264-[YTS.AG].CZ.mkv",
+            "2000000000",
+            "2",
+            "0",
+          ),
+          file(
+            "5",
+            "The.LongTrain.1999.1080p.BluRay.x264-[YTS.AG].EN.mkv",
+            "2000000000",
+            "1",
+            "0",
+          ),
+        ],
+      },
+    }));
+
+    const results = await webshare.search(showInfo, null);
+    expect(results.length).toBe(5);
+    // Check that language information is correctly extracted
+    expect(results[0].description).not.toContain("🌐");
+    expect(results[1].description).toContain("🌐 CZ|EN");
+    expect(results[2].description).toContain("🌐 CZ|EN");
+    expect(results[3].description).toContain("🌐 CZ");
+    expect(results[4].description).toContain("🌐 EN");
+  });
+});
